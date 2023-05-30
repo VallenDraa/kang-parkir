64404b8568c7a6d3de14d1f0002fd46a8fccdecb
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-05-30 19:44:52 +0700

    Menambahkan chart.js untuk menampilkan grafik kolom

--- components/head-tags.php
+++ components/head-tags.php
@@ -1,1 +1,4 @@
-  <script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.tailwindcss.com"></script>
+<script defer type="module">
+  import chartJs from 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/+esm'
+</script>
