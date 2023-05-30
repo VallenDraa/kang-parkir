97f3bf71f254ccc8724ef2a6ee251ecec797d565
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-05-31 02:42:28 +0700

    Menambahkan FontAwesome untuk icons

--- components/head-tags.php
+++ components/head-tags.php
@@ -2,3 +2,4 @@
 <script defer type="module">
   import chartJs from 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/+esm'
 </script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
