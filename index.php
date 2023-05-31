7a347c62b5cd15301e28bfcca0412cef7373a8bd
NashifRahman <nashifrahman2004@gmail.com>
2023-05-31 12:58:58 +0700

    test tailwind css and war crime

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include "./components/head-tags.php";
+    ?>
+</head>
+
+<body>
+    <h1 class="text-blue-500 text-xl lg:text-green-700">Hello World</h1>
+    <div class="h-16 w-36 bg-pink-700 rounded-full shadow-xl shadow-neutral-950">
+
+    </div>
+</body>
+
+</html>
