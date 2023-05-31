ca82920441a82999d80e36fcda0ca5cfc04415d0
Fhrizayd <faryuuraksu@gmail.com>
2023-05-31 12:55:45 +0700

    menambahkan tailwind pada index.phpi

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include "./components/head-tags.php";
+    ?>
+
+</head>
+
+<body>
+    <h1 class="text-blue-500 text-9xl lg:text-green-400 ">HEllO world</h1>
+
+    <div class= "h-36 w-36 bg-amber-100 rounded-2xl shadow-md shadow-emerald-900">
+
+    </div>
+    <!-- #region -->
+</body>
+
+</html>
