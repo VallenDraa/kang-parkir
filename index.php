0ca6420f8401296beaedaeb055bcaba229a6c3b4
TanjungArs <arswendo123@gmail.com>
2023-05-31 12:56:12 +0700

    Menambahkan tailwind di bagian index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    include "./components/head-tags.php";
+    ?>
+    <title>Document</title>
+</head>
+
+<body>
+    <h1 class="text-rose-400 text-3xl">Hello World</h1>
+    <div class="h-36 w-52 bg-lime-700 rounded-xl shadow-2xl shadow-red-700"></div>
+</body>
+</html>
