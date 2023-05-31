0c7cd73629a32e4368f02c74d980279868064c8f
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-05-31 12:02:06 +0700

    Mempersiapkan folder admin, tooltip, dan file sql

--- components/head-tags.php
+++ components/head-tags.php
@@ -1,5 +1,14 @@
+<!-- tailwind -->
 <script src="https://cdn.tailwindcss.com"></script>
+
+<!-- chart/grafik -->
 <script defer type="module">
   import chartJs from 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/+esm'
 </script>
+
+<!-- font awesome icon -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+<!-- tooltip -->
+<script src="https://unpkg.com/@popperjs/core@2"></script>
+<script src="https://unpkg.com/tippy.js@6"></script>
