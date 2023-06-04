764a179f7f2a431960b8388c3c117283ffdc0a67
NashifRahman <nashifrahman2004@gmail.com>
2023-06-04 12:00:32 +0700

    mengganti data

---
+++ ganti.php
@@ -0,0 +1,24 @@
+<body>
+    <?php
+    session_start();
+    $id = $_SESSION["id"];
+    ?>
+    <h2>Edit Data</h2>
+    <form action="ganti2.php" method="post">
+        <table width="487" border="0">
+            <input type="hidden" name="id" value="<?php echo $id; ?>" />
+            <tr>
+                <td width="150">Username</td>
+                <td width="327"><input type="text" name="username" value="<?php echo $_SESSION["username"] ?>"></td>
+            </tr>
+            <tr>
+                <td width="150">Password</td>
+                <td width="327"><input type="text" name="password" value="<?php echo $_SESSION["password"] ?>"></td>
+            </tr>
+            <tr>
+                <td height="68" align="right"><input type="reset" value="reset"></td>
+                <td><input type="submit" value="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
---
+++ user.php
@@ -0,0 +1,46 @@
+<?php
+include "db/koneksi.php";
+$hasil = mysqli_query($conn, "SELECT * FROM user");
+$buff = mysqli_fetch_assoc($hasil);
+
+session_start();
+$_SESSION["id"] = $buff["id"];
+$_SESSION["username"] = $buff["username"];
+$_SESSION["password"] = $buff["password"];
+
+$id = $_SESSION["id"];
+
+?>
+<script src="https://cdn.tailwindcss.com"></script>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>HALAMAN USER</h1>
+    </BR>
+    <div class="flex font-sans text-xl">
+        <div>
+            <input type="hidden" name="id">
+            <h2>Username</h2>
+            <h2>Password</h2>
+        </div>
+        <div class="pl-3">
+            <input type="hidden" value="<?php echo $id ?>">
+            <h2>: <?php echo $_SESSION["username"] ?></h2>
+            <h2>: <?php echo $_SESSION["password"] ?></h2>
+        </div>
+    </div>
+    </br>
+    <div class="pl-3">
+        <button class="rounded-none h-5 w-20 bg-slate-500 "><a href="ganti.php?id=<?php echo $_SESSION["id"] ?>">Ganti</a></button>
+    </div>
+</body>
+
+</html>
