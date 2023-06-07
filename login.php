cc4a6d936f14d11faa729b0f5c35704bd2f662d6
NashifRahman <nashifrahman2004@gmail.com>
2023-06-07 15:49:18 +0700

    membuat prototype halaman login

--- login.php
+++ login.php
@@ -1,28 +1,42 @@
 <html lang="en">
+
 <head>
-    Halaman Login
+    <?php
+    include "./components/head-tags.php";
+    ?>
+    <title>Halaman Login</title>
 </head>
 
-<body>
-<form action="lib/loginproc.php" method="post">
-    <tr>
-			USERNAME
-				<input type="text" name="username" size="17" />
+<body class="h-full">
+    <div class="container flex justify-center items-center mx-auto h-full">
+        <form action="?php=loginproc#pos" method="post" class="bg-blue-600 text-gray-200 w-fit gap-3 p-10 rounded-lg">
+            <h2 class="text-center mb-3">Login Untuk Masuk</h2>
 
-			PASSWORD
-				<input type="password" name="password" size="17"/>
-	</tr>
+            <div class="flex flex-col items-center">
+                <div>
+                    <label for="username" class="block mb-1">
+                        USERNAME
+                    </label>
+                    <i class="fa-solid fa-user"></i>
+                    <input id="username" type="text" name="username" placeholder="USERNAME" required class="border border-gray-200 py-1 pr-4 pl-10 bg-blue-600 rounded-lg" />
+                </div>
 
-    <tr>
-        <label for="id">
-            <input type="checkbox" id="isAdmin" name="checklist" value="isAdmin"> Admin
-        </label>
-    </tr>
+                <div class="mb-3">
+                    <label for="password" class="block mb-1">
+                        PASSWORD
+                    </label>
+                    <i class="fa-solid fa-key"></i>
+                    <input id="password" type="password" name="pass" placeholder="PASSWORD" required class="border border-gray-200 py-1 pr-4 pl-10 bg-blue-600 rounded-lg" />
+                </div>
 
-    <tr>
-        <input type="submit" value="Login" size="16"/>
-    </tr>
-</form>
+                <div>
+                    <input type="checkbox" name="check" id="check"> Login as Admin
+                </div>
+
+                <button class="border w-full border-gray-200 py-1 bg-white text-blue-600 rounded-lg">Login</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
