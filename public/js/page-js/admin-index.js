77a79a8c1ace973dde6ec23770d826c2906c0693
VallenDraa <jestinevallendradwiputra@gmail.com>
2023-06-07 19:19:15 +0700

    Menambahkan fitur pengkondisian checkbox ketika menambah plat

--- public/js/page-js/admin-index.js
+++ public/js/page-js/admin-index.js
@@ -5,14 +5,22 @@ const submitMotorBtn = qs("#submit-motor-btn");
 const hapusMotorforms = qsa("#hapus-motor-form");
 const inputPlat = qs("[name='plat-motor']");
 
+/** @type {HTMLInputElement} */
+const platUserBaruCheckbox = qs("[name='plat-user-baru']");
+
+/** @type {HTMLSelectElement} */
+const platUserLamaSelection = qs("[name='plat-user-lama']");
+
 /** @type {HTMLDialogElement} */
 const actionDialog = qs("#action-dialog");
 const closeActionDialogBtn = qs("#close-action-dialog-btn");
 
+// tampilkan modal ketika tombol tambah motor ditekan
 tambahMotorBtn?.addEventListener("click", () => {
   actionDialog?.showModal();
 });
 
+// konfirmasi ketika menghapus motor
 hapusMotorforms?.forEach(form => {
   form.addEventListener("submit", e => {
     e.preventDefault();
@@ -25,12 +33,23 @@ hapusMotorforms?.forEach(form => {
   });
 });
 
+// tutup modal ketika mengsubmit motor baru
 submitMotorBtn?.addEventListener("click", () => {
   if (inputPlat.value !== "") {
     actionDialog?.close();
   }
 });
 
+// tutup modal ketika user meneka tombol x pada modal
 closeActionDialogBtn?.addEventListener("click", () => {
   actionDialog?.close();
+});
+
+// matikan opsi untuk memasukkan plat ke user lama
+platUserBaruCheckbox?.addEventListener("change", e => {
+  if (e.target.checked) {
+    platUserLamaSelection.setAttribute("disabled", e.target.checked);
+  } else {
+    platUserLamaSelection.removeAttribute("disabled");
+  }
 });
