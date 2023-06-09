240379a445504673ab5815416ee8aafe3d5590ae
NashifRahman <nashifrahman2004@gmail.com>
2023-06-09 16:01:31 +0700

    Front-end halaman login

--- login.php
+++ login.php
@@ -9,34 +9,37 @@
 
 <body class="h-full">
     <div class="container flex justify-center items-center mx-auto h-full">
-        <form action="?php=loginproc#pos" method="post" class="bg-blue-600 text-gray-200 w-fit gap-3 p-10 rounded-lg">
+        
+        <form action="?php=loginproc#pos" method="post" class="bg-blue-500 text-gray-200 w-fit gap-3 p-10 rounded-lg">
             <h2 class="text-center mb-3">Login Untuk Masuk</h2>
 
-            <div class="flex flex-col items-center">
-                <div>
-                    <label for="username" class="block mb-1">
-                        USERNAME
-                    </label>
+            <div class="flex flex-col items-center ">
+                <div class="flex items-center relative border border-gray-200 pl-3 rounded-lg mb-3">
                     <i class="fa-solid fa-user"></i>
-                    <input id="username" type="text" name="username" placeholder="USERNAME" required class="border border-gray-200 py-1 pr-4 pl-10 bg-blue-600 rounded-lg" />
+
+                    <input type="text" id="username" placeholder="Username" class="transition-colors placeholder:text-transparent peer py-1 w-full rounded-md px-10 pl-4 text-lg text-gray-200   outline-none bg-transparent disabled:cursor-not-allowed disabled:opacity-20 border-l-0 rounded-l-none">
+
+                    <label class="px-1 -translate-x-1 scale-90 transition-all absolute left-9 top-1/2 -translate-y-9 text-sm text-gray-200  bg-blue-500 peer-focus:-translate-x-8 peer-focus:-translate-y-[30px] peer-focus:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:translate-x-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-gray-200" for=" username"><span>Username</span></label>
                 </div>
 
-                <div class="mb-3">
-                    <label for="password" class="block mb-1">
-                        PASSWORD
-                    </label>
+                <div class="flex items-center relative border border-gray-200 pl-3 rounded-lg mb-2">
                     <i class="fa-solid fa-key"></i>
-                    <input id="password" type="password" name="pass" placeholder="PASSWORD" required class="border border-gray-200 py-1 pr-4 pl-10 bg-blue-600 rounded-lg" />
+
+                    <input type="password" id="pass" placeholder="Password" class="transition-colors placeholder:text-transparent peer py-1 w-full rounded-md px-10 pl-4 text-lg text-gray-200   outline-none bg-transparent disabled:cursor-not-allowed disabled:opacity-20 border-l-0 rounded-l-none">
+
+                    <label class="px-1 -translate-x-1 scale-90 transition-all absolute left-9 top-1/2 -translate-y-9 text-sm text-gray-200  bg-blue-500 peer-focus:-translate-x-8 peer-focus:-translate-y-[30px] peer-focus:scale-90 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:translate-x-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:text-gray-200" for=" pass"><span>Password</span></label>
                 </div>
 
-                <div>
+                <div class="mb-2">
                     <input type="checkbox" name="check" id="check"> Login as Admin
                 </div>
 
-                <button class="border w-full border-gray-200 py-1 bg-white text-blue-600 rounded-lg">Login</button>
+                <button class="border w-full border-gray-200 py-1 bg-white text-blue-500 rounded-lg drop-shadow-lg">Login</button>
             </div>
         </form>
     </div>
 </body>
 
 </html>
+
+<!-- focus:border-transparent -->
